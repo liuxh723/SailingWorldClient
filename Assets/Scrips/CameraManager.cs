76b77b00053755c6af322f9908da7a1cc8530736
liuxh723 <86610076@qq.com>
2018-05-22 23:18:51 +0800

    earth

--- Assets/Scrips/CameraManager.cs
+++ Assets/Scrips/CameraManager.cs
@@ -6,6 +6,7 @@ public class CameraManager : MonoBehaviour {
 
     public Transform lookAtTarget;
     public Transform frameTarget;
+    public float harf = 1000.0f;
     public float damping = 2.0f;
     public bool GoLookAtTarget = false;
     void Update()
@@ -14,6 +15,8 @@ public class CameraManager : MonoBehaviour {
         {
             if (!lookAtTarget || !frameTarget)
                 return;
+            Vector3 position = new Vector3(harf * Mathf.Sin(0.7854f) * Mathf.Cos(0.7854f), harf * Mathf.Sin(0.7854f) * Mathf.Sin(0.7854f), harf * Mathf.Cos(0.7854f));
+            transform.position = position;
             Quaternion rotate = Quaternion.LookRotation(lookAtTarget.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotate, Time.deltaTime * damping);
         }
